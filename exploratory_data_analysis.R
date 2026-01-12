2bfd950831bbb1e6512a199ba05e1b495623917e
jacobmchen <jacobchen01@gmail.com>
2026-01-12 11:50:09 -0500

    finished exploratory data analysis on biomarkers and creatinine

--- exploratory_data_analysis.R
+++ exploratory_data_analysis.R
@@ -12,32 +12,52 @@ library(tidyverse)
 library(readxl)
 
 # read the mediation data
-mediation_data <- read.csv("mediation_data_final_original.csv")
+mediation_data <- read.csv("analysis_data_11192025.csv")
 
 # subset to patients that experienced AKI and look only at
 # change in NGAL and KIM-1
 filtered_data <- mediation_data %>%
     filter(aki == 1) %>%
-    select(delta_KIM_1, delta_NGAL)
+    select(PID, delta_KIM.1, delta_NGAL)
+
+# save change in KIM-1 and NGAL as histograms
+png("plots/kim1.png")
+hist(filtered_data$delta_KIM.1, main="Change in KIM-1 at X-Clamp Off Among\nAKI Patients",
+     xlab="Change in KIM-1 at X-Clamp Off Compared to Baseline")
+png("plots/ngal.png")
+hist(filtered_data$delta_NGAL, main="Change in NGAL at X-Clamp Off Among\nAKI Patients",
+     xlab="Change in NGAL at X-Clamp Off Compared to Baseline")
 
 # print their average delta_KIM-1 and delta_NGAL
 print("sample mean KIM-1 change")
-print(mean(filtered_data$delta_KIM_1))
+print(mean(filtered_data$delta_KIM.1, na.rm=TRUE))
 print("sample standard deviation KIM-1")
-print(sd(filtered_data$delta_KIM_1))
+print(sd(filtered_data$delta_KIM.1, na.rm=TRUE))
 print("sample mean NGAL change")
-print(mean(filtered_data$delta_NGAL))
+print(mean(filtered_data$delta_NGAL, na.rm=TRUE))
 print("sample standard deviation NGAL change")
-print(sd(filtered_data$delta_NGAL))
+print(sd(filtered_data$delta_NGAL, na.rm=TRUE))
 
 # read the raw creatinine data
 creatinine_data <- read_excel("../../../KDIGO-AKI.xlsx")
 
 # subset to patients that experienced AKI and look only at
 # change in creatinine 48 hours post-surgery
-filtered_data <- creatinine_data %>%
+creatinine_data <- creatinine_data %>%
     filter(KDIGO_AKI == "YES") %>%
-    select(48hrs_delCr)
+    select(PID, `48hrs_delCr`)
+
+# merge the patients from the biomarkers data to make sure
+# that the patients experiencing AKI are the same in both datasets
+filtered_data <- full_join(filtered_data, creatinine_data, by="PID")
+
+print("sample mean change in creatinine")
+print(mean(filtered_data$`48hrs_delCr`))
+print("sample standard deviation change in creatinine")
+print(sd(filtered_data$`48hrs_delCr`))
 
 # plot a histogram of the change in creatinine data
-hist(filtered_data$48hrs_delCr)
+png("plots/creatinine.png")
+hist(filtered_data$`48hrs_delCr`, main="Change in Creatinine 48 Hours After Surgery\nAmong AKI Patients",
+     xlab="Change in Creatinine 48 Hours After Surgery Compared to Baseline")
+dev.off()
